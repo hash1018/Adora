249c3c712698b7e12dc98eb17ab3e4a2358917e1
hash1018 <46293206+hash1018@users.noreply.github.com>
2021-03-18 06:51:55 +0900

    update HotkeyLineEdit

--- Adora/Adora/Ui/Item/HotkeyLineEdit.cpp
+++ Adora/Adora/Ui/Item/HotkeyLineEdit.cpp
@@ -3,6 +3,8 @@
 #include "HotkeyLineEdit.h"
 #include <QKeyEvent>
 
+QList<QKeySequence> HotkeyLineEdit::lists;
+
 HotkeyLineEdit::HotkeyLineEdit(QWidget *parent)
 	:QLineEdit(parent) {
 
@@ -17,12 +19,46 @@ HotkeyLineEdit::~HotkeyLineEdit() {
 void HotkeyLineEdit::keyPressEvent(QKeyEvent *event) {
 
 	if (event->key() >= Qt::Key_Space && event->key() <= Qt::Key_AsciiTilde) {
+
+		int index = HotkeyLineEdit::lists.indexOf(QKeySequence(event->modifiers() + event->key()));
+
+		if (index == -1) {
+		
+			HotkeyLineEdit::lists.append(QKeySequence(event->modifiers() + event->key()));
+			
+			if (HotkeyLineEdit::lists.indexOf(this->keySequence) != -1)
+				HotkeyLineEdit::lists.removeAt(HotkeyLineEdit::lists.indexOf(this->keySequence));
+
+			this->keySequence = QKeySequence(event->modifiers() + event->key());
+		}
+		else {
+		
+			return;
+		}
+
 		this->setText(QKeySequence(event->modifiers() + event->key()).toString());
 
 		emit this->hotkeyEmitted(event->modifiers(), event->key());
 
 	}
 	else {
+
+		int index = HotkeyLineEdit::lists.indexOf(QKeySequence(event->modifiers()));
+
+		if (index == -1) {
+
+			HotkeyLineEdit::lists.append(QKeySequence(event->modifiers()));
+
+			if (HotkeyLineEdit::lists.indexOf(this->keySequence) != -1)
+				HotkeyLineEdit::lists.removeAt(HotkeyLineEdit::lists.indexOf(this->keySequence));
+
+			this->keySequence = QKeySequence(event->modifiers());
+		}
+		else {
+
+			return;
+		}
+
 		QString str = QKeySequence(event->modifiers()).toString();
 		int pos = str.lastIndexOf('+');
 		str = str.left(pos);
--- Adora/Adora/Ui/Item/HotkeyLineEdit.h
+++ Adora/Adora/Ui/Item/HotkeyLineEdit.h
@@ -5,9 +5,16 @@
 
 #include <qlineedit.h>
 
+#include <qlist.h>
+
 class HotkeyLineEdit : public QLineEdit {
 
 	Q_OBJECT
+
+private:
+	static QList<QKeySequence> lists;
+
+	QKeySequence keySequence;
 
 public:
 	HotkeyLineEdit(QWidget *parent = nullptr);
