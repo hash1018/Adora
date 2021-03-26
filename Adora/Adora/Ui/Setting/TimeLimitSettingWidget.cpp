db40424482b5d5a7fd8a84f602dae81b70d063fd
hash1018 <46293206+hash1018@users.noreply.github.com>
2021-03-26 21:14:30 +0900

    modified spinBox

--- Adora/Adora/Ui/Setting/TimeLimitSettingWidget.cpp
+++ Adora/Adora/Ui/Setting/TimeLimitSettingWidget.cpp
@@ -2,21 +2,33 @@
 
 #include "TimeLimitSettingWidget.h"
 #include "Base/SettingManager.h"
+#include "Base/LanguageManager.h"
+
 TimeLimitSettingWidget::TimeLimitSettingWidget(QWidget *parent)
 	:AbstractStackWidget(parent) {
 
 	ui.setupUi(this);
 
-	ui.useTimeLimitCheckBox->setChecked(SettingManager::getInstance()->getTimeLimitSetting()->getUseTimeLimit());
+	bool checked = SettingManager::getInstance()->getTimeLimitSetting()->getUseTimeLimit();
+	ui.useTimeLimitCheckBox->setChecked(checked);
 	ui.hourSpinBox->setValue(SettingManager::getInstance()->getTimeLimitSetting()->getHour());
 	ui.minuteSpinBox->setValue(SettingManager::getInstance()->getTimeLimitSetting()->getMinute());
 	ui.secondSpinBox->setValue(SettingManager::getInstance()->getTimeLimitSetting()->getSecond());
+
+	ui.hourSpinBox->setDisabled(!checked);
+	ui.minuteSpinBox->setDisabled(!checked);
+	ui.secondSpinBox->setDisabled(!checked);
 
 
 	connect(ui.useTimeLimitCheckBox, &QCheckBox::toggled, this, &TimeLimitSettingWidget::useTimeLimitCheckBoxToggled);
 	connect(ui.hourSpinBox, SIGNAL(valueChanged(int)), this, SLOT(hourSpinBoxValueChanged(int)));
 	connect(ui.minuteSpinBox, SIGNAL(valueChanged(int)), this, SLOT(minuteSpinBoxValueChanged(int)));
 	connect(ui.secondSpinBox, SIGNAL(valueChanged(int)), this, SLOT(secondSpinBoxValueChanged(int)));
+
+
+	QString str = "<b>" + getLanUiValue("MenuTimeLimit/TimeLimit") + "</b>";
+	ui.timeLimitLabel->setTextFormat(Qt::RichText);
+	ui.timeLimitLabel->setText(str);
 }
 
 TimeLimitSettingWidget::~TimeLimitSettingWidget() {
@@ -26,6 +38,10 @@ TimeLimitSettingWidget::~TimeLimitSettingWidget() {
 void TimeLimitSettingWidget::useTimeLimitCheckBoxToggled(bool checked){
 
 	SettingManager::getInstance()->getTimeLimitSetting()->setUseTimeLimit(checked);
+
+	ui.hourSpinBox->setDisabled(!checked);
+	ui.minuteSpinBox->setDisabled(!checked);
+	ui.secondSpinBox->setDisabled(!checked);
 }
 
 void TimeLimitSettingWidget::hourSpinBoxValueChanged(int value) {
