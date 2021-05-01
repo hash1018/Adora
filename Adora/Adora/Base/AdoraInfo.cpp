660e09c21fad6a3d993ff676ce059e244750165e
hash1018 <46293206+hash1018@users.noreply.github.com>
2021-05-02 08:56:01 +0900

    modified version

--- Adora/Adora/Base/AdoraInfo.cpp
+++ Adora/Adora/Base/AdoraInfo.cpp
@@ -3,7 +3,7 @@
 #include "AdoraInfo.h"
 
 AdoraInfo::AdoraInfo()
-	:version("1.0.0"), email("sh.ha1018@gmail.com"), github("www.github.com/hash1018")
+	:version("1.0.1"), email("sh.ha1018@gmail.com"), github("www.github.com/hash1018")
 	, name("Seungho Ha") {
 
 }
